bf045f7b1e20313f1869fd047504010149094ee5
bokeum <bokmail83@gmail.com>
2025-02-11 10:11:19 +0900

    판다 이미지 적용용

--- src/data/characters.js
+++ src/data/characters.js
@@ -1,19 +1,41 @@
 // src/data/characters.js
 
+// 캐릭터 이미지 가져오기 (0~25번까지 총 26개)
 import character00 from '/assets/character00.webp';
 import character01 from '/assets/character01.webp';
 import character02 from '/assets/character02.webp';
 import character03 from '/assets/character03.webp';
 import character04 from '/assets/character04.webp';
 import character05 from '/assets/character05.webp';
+import character06 from '/assets/character06.webp';
+import character07 from '/assets/character07.webp';
+import character08 from '/assets/character08.webp';
+import character09 from '/assets/character09.webp';
+import character10 from '/assets/character10.webp';
+import character11 from '/assets/character11.webp';
+import character12 from '/assets/character12.webp';
+import character13 from '/assets/character13.webp';
+import character14 from '/assets/character14.webp';
+import character15 from '/assets/character15.webp';
+import character16 from '/assets/character16.webp';
+import character17 from '/assets/character17.webp';
+import character18 from '/assets/character18.webp';
+import character19 from '/assets/character19.webp';
+import character20 from '/assets/character20.webp';
+import character21 from '/assets/character21.webp';
+import character22 from '/assets/character22.webp';
+import character23 from '/assets/character23.webp';
+import character24 from '/assets/character24.webp';
+import character25 from '/assets/character25.webp';
 
+// 이미지 배열 생성 (0~25번까지 포함)
 const characters = [
-  character00,
-  character01,
-  character02,
-  character03,
-  character04,
-  character05,
+  character00, character01, character02, character03, character04,
+  character05, character06, character07, character08, character09,
+  character10, character11, character12, character13, character14,
+  character15, character16, character17, character18, character19,
+  character20, character21, character22, character23, character24,
+  character25
 ];
 
 export default characters;
